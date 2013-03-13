d187fed0ff2ddc645531e1e3f304b110ace12277
Casey Robinson <kc@rampantmonkey.com>
2013-03-13 09:53:35 -0400

    extract file finding method

--- lib/menagerie_generator/runner.rb
+++ lib/menagerie_generator/runner.rb
@@ -9,17 +9,7 @@ module MenagerieGenerator
     end
 
     def run
-      time_series = []
-      summaries = []
-      Pathname.glob(@source + "log-rule*") do |path|
-        if path.to_s.match /.*summary/
-          summaries << path
-        else
-          time_series << path
-        end
-      end
-      @time_series = time_series
-      @summaries = summaries
+      find_files
     end
 
     private
@@ -27,6 +17,20 @@ module MenagerieGenerator
         fail ::ArgumentError unless args.length > 1
         @source = Pathname.new args[0]
         @destination = Pathname.new args[1]
+      end
+
+      def find_files
+        time_series = []
+        summaries = []
+        Pathname.glob(@source + "log-rule*") do |path|
+          if path.to_s.match /.*summary/
+            summaries << path
+          else
+            time_series << path
+          end
+        end
+        @time_series = time_series
+        @summaries = summaries
       end
   end
 end
