18e3a1b19a6ed964030cc767051a65682ed6b3c6
Casey Robinson <kc@rampantmonkey.com>
2013-03-13 12:49:01 -0400

    send format to gnuplot to create histograms.

--- lib/menagerie_generator/runner.rb
+++ lib/menagerie_generator/runner.rb
@@ -53,7 +53,8 @@ module MenagerieGenerator
 
       def create_histograms
         @maximums = find_maximums
-        write_maximum_resources
+        write_maximum_values
+        build_histograms
       end
 
       def find_maximums
@@ -69,13 +70,47 @@ module MenagerieGenerator
         max
       end
 
-      def write_maximum_resources
+      def write_maximum_values
         @maximums.each do |m|
           path = @destination + m.first.to_s
           File.open(path, 'w:UTF-8') do |f|
             m.last.each { |line| f.puts line }
           end
         end
+      end
+
+      def build_histograms
+        @resources.each do |r|
+          gnuplot {|io| io.puts histogram_format(resource: r, data_path: @destination+r.to_s)}
+        end
+      end
+
+      def gnuplot
+        output = nil
+        IO::popen "gnuplot", "w+" do |io|
+          yield io
+          io.close_write
+          output = io.read
+        end
+        output
+      end
+
+      def histogram_format(width: 600, height: 600, resource: "", data_path: "/tmp")
+        %Q{set terminal png size #{width},#{height}
+        set bmargin 4
+        set style line 1
+        unset key
+
+        set ylabel "Frequency"
+        set output "#{@destination + resource.to_s}_hist.png"
+        binwidth=1
+        set boxwidth binwidth*0.9
+        set style fill solid 0.5
+        bin(x,width)=width*floor(x/width)
+        set yrange [0:*]
+        set xlabel "#{resource.to_s}"
+        plot "#{data_path.to_s}" using (bin(\$1,binwidth)):(1.0) smooth freq with boxes
+        }
       end
   end
 end
