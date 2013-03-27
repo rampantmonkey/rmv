20f7dfbd6a21bf7b7455376c73a77139fddf3f9c
Casey Robinson <kc@rampantmonkey.com>
2013-03-27 16:35:50 -0400

    help option

--- lib/menagerie_generator/options.rb
+++ lib/menagerie_generator/options.rb
@@ -18,8 +18,20 @@ module MenagerieGenerator
     def parse argv
       OptionParser.new do |opts|
         opts.banner = "Usage:    menagerie [options] "
+        opts.on("-h", "--help", "Show this message") do
+          puts opts
+          exit
+        end
+        begin
+          argv = ["-h"] if argv.empty?
+          opts.parse! argv
+        rescue OptionParser::ParseError => e
+          STDERR.puts e.message, "\n", opts
+          exit(-1)
+        end
       end
     end
+
     private
       attr_accessor :config
   end
