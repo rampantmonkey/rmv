abefc5cacedc11f51d1e88d6dbdd81f2b798976a
Casey Robinson <kc@rampantmonkey.com>
2013-03-27 13:41:35 -0400

    add class to build histograms

--- lib/menagerie_generator.rb
+++ lib/menagerie_generator.rb
@@ -4,6 +4,7 @@ require "menagerie_generator/makeflow_log"
 require "menagerie_generator/summary"
 require "menagerie_generator/summary_collection"
 require "menagerie_generator/resources"
+require "menagerie_generator/histogram_builder"
 
 module MenagerieGenerator
 end
---
+++ lib/menagerie_generator/histogram_builder.rb
@@ -0,0 +1,6 @@
+require_relative '../menagerie_generator'
+
+module MenagerieGenerator
+  class HistogramBuilder
+  end
+end
