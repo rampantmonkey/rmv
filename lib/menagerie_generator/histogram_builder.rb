72c5246a750ffabd45768dcfc45208cdd1e050da
Casey Robinson <kc@rampantmonkey.com>
2013-03-27 13:44:03 -0400

    define the inputs to histogram builder

--- lib/menagerie_generator/histogram_builder.rb
+++ lib/menagerie_generator/histogram_builder.rb
@@ -2,5 +2,14 @@ require_relative '../menagerie_generator'
 
 module MenagerieGenerator
   class HistogramBuilder
+    def initialize resources, summaries, workspace, destination
+      @resources = resources
+      @summaries = summaries
+      @workspace = workspace
+      @destination = destination
+    end
+
+    private
+      attr_reader :resources, :summaries, :workspace, :destination
   end
 end
