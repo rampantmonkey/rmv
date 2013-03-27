daa24be67d0e23ace6601db6032c1d76f44b012a
Casey Robinson <kc@rampantmonkey.com>
2013-03-27 13:46:16 -0400

    move gnuplot information into histogram

--- lib/menagerie_generator/histogram_builder.rb
+++ lib/menagerie_generator/histogram_builder.rb
@@ -22,5 +22,31 @@ module MenagerieGenerator
 
     private
       attr_reader :resources, :summaries, :workspace, :destination
+      def gnuplot_format(width: 600, height: 600, resource: "", data_path: "/tmp", group: 0)
+        max = scale_maximum resource.name.to_s, @grouped_maximums[group][resource].max
+        unit = resource.unit
+        image_path = destination + "group#{group}"
+        image_path.mkpath
+        image_path += "#{resource.name.to_s}_#{width}x#{height}_hist.png"
+        binwidth = 1
+        binwidth = max/40 unless max <= 40
+        %Q{set terminal png transparent size #{width},#{height}
+        set bmargin 4
+        unset key
+
+        set ylabel "Frequency"
+        set output "#{image_path}"
+        binwidth=#{binwidth}
+        set boxwidth binwidth*0.9
+        set style fill solid 0.5
+        bin(x,width)=width*floor(x/width)
+        set yrange [0:*]
+        set xrange [0:*]
+        set xtics right rotate by -45
+        set xlabel "#{resource.name.to_s}#{unit}" offset 0,-2 character
+        set bmargin 7
+        plot "#{data_path.to_s}" using (bin(\$1,binwidth)):(1.0) smooth freq w boxes lc rgb"#5aabbc"
+        }
+      end
   end
 end
