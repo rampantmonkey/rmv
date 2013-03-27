4bc805fc770e9d75ad94754e5b6e32998d09eb95
Casey Robinson <kc@rampantmonkey.com>
2013-03-27 16:33:54 -0400

    specify default configuration options

--- lib/menagerie_generator/options.rb
+++ lib/menagerie_generator/options.rb
@@ -1,11 +1,17 @@
 require_relative '../menagerie_generator'
 
 require 'optparse'
+require 'pathname'
 
 module MenagerieGenerator
   class Options
 
     def initialize argv
+      @config = {source: nil,
+                 debug: false,
+                 destination: nil,
+                 name: "unnamed",
+                 workspace: Pathname.new("/tmp/menagerie")}
     end
     private
       attr_accessor :config
