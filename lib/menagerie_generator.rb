eeb5e39d3d7d60837e799f2bf48f317173d233c7
Casey Robinson <kc@rampantmonkey.com>
2013-03-27 12:49:35 -0400

    class to hold information about resources
    Should make this a delegate at some point, rather than doing it by hand.

--- lib/menagerie_generator.rb
+++ lib/menagerie_generator.rb
@@ -3,6 +3,7 @@ require "menagerie_generator/runner"
 require "menagerie_generator/makeflow_log"
 require "menagerie_generator/summary"
 require "menagerie_generator/summary_collection"
+require "menagerie_generator/resources"
 
 module MenagerieGenerator
 end
---
+++ lib/menagerie_generator/resources.rb
@@ -0,0 +1,42 @@
+require_relative '../menagerie_generator'
+
+module MenagerieGenerator
+  class Resources
+    def initialize  header
+      @r = header.split(/\s+/).map do |h|
+        unit = h.scan(/\((.*)\)/)[0]
+        unit = unit.first if unit
+        unit = "" unless unit
+        name = translate_resource_name h.gsub(/\(.*\)/, '')
+        Resource.new name, unit
+      end
+    end
+
+    def map &b
+      @r.map &b
+    end
+
+    def each &b
+      @r.each &b
+    end
+
+    def index v
+      @r.index v
+    end
+
+    def each_with_index &b
+      @r.each_with_index &b
+    end
+
+    private
+      class Resource < Struct.new(:name, :unit)
+        def to_s
+          name.to_s
+        end
+      end
+
+      def translate_resource_name name
+        name.gsub /clock/, 'time'
+      end
+  end
+end
