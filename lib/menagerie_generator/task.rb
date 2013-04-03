249d58acd277e66ca62adead7e294e0bcceaa49e
Casey Robinson <kc@rampantmonkey.com>
2013-04-03 16:30:59 -0400

    initialize task

--- lib/menagerie_generator/task.rb
+++ lib/menagerie_generator/task.rb
@@ -2,5 +2,18 @@ require_relative '../menagerie_generator'
 
 module MenagerieGenerator
   class Task
+    def initialize summary_path, time_series_path
+      @summary_path = summary_path
+      @summary = load_summary
+      @time_series_path = time_series_path
+    end
+
+
+    private
+      attr_reader :summary, :summary_path, :time_series_path
+
+      def load_summary
+        Summary.new (YAML.load_file summary_path)
+      end
   end
 end
