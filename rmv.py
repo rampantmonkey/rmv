519466444ecb4bf7144199bcc15cfee14e89929c
Casey Robinson <kc@rampantmonkey.com>
2013-07-01 13:53:51 -0400

    get command line arguments

--- rmv.py
+++ rmv.py
@@ -4,7 +4,14 @@
 ## find gnuplot version
 import os
 GNUPLOT_VERSION = float(os.popen("gnuplot --version | awk '{print $2}'").read())
+
 ## get args
+import argparse
+option_parser = argparse.ArgumentParser(description='Visualize resource monitor data')
+option_parser.add_argument("source", help="the directory containing your data")
+option_parser.add_argument("destination", help="the desired output directory")
+args = option_parser.parse_args()
+
 ## pull source and destination from args
 ## assert source exists
 ## create destination base directory
