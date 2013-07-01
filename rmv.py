83e13414493734fce8ef50b536faef870d5c93a1
Casey Robinson <kc@rampantmonkey.com>
2013-07-01 13:46:45 -0400

    outline of tasks.
    Taken from ruby implementation.

--- rmv.py
+++ rmv.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python
 
+# initialize
+## find gnuplot version
+## get args
+## pull source and destination from args
+## assert source exists
+## create destination base directory
+
+# run
+## find files
+## find resources
+## create histograms
+## create group resource summaries
+## make combined time series
+## plot makeflow log
+## copy static files
+## clean up temporary files
